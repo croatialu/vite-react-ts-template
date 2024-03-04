971f63c9d74ed5e4dc5d6bb2946d64c5b072438b
croatialu <wuli.croatia@foxmail.com>
2024-03-04 12:39:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + TypeScript + Vite + TS
+
+- Vite, React, TS
+- React Router
+- eslint @antfu/eslint-config
+- iconify
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
+import App from './App.tsx'
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
